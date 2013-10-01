1ae708ce1b47e79c6e1d37f0c19f9f856a85dc86
Ernado <ernado@cygame.ru>
2013-10-01 20:58:20 +0400

    removed unused files fixed log format change pgpass return

--- backuper/database.py
+++ backuper/database.py
@@ -1,4 +1,8 @@
 # coding=utf-8
+"""
+Database dump module
+"""
+
 import os
 import subprocess
 from ConfigParser import ConfigParser, NoSectionError
@@ -7,14 +11,21 @@ from config import get_config
 import log
 
 logger = log.get(__name__)
-password_format = 'localhost:5432:db_{name}:pg_{name}:{key}\n'
-command_format = 'pg_dump -U pg_{name} db_{name}'
+password_string = 'localhost:5432:db_{name}:pg_{name}:{key}\n'
+command_string = 'pg_dump -U pg_{name} db_{name}'
 
 
 def dump(project_name, dump_file=None):
+    """
+    Dump database of project
+    @type dump_file: file
+    @param project_name: name of project's folder
+    @param dump_file: output file
+    @type project_name: str
+    """
     logger.info('Dumping %s database' % project_name)
 
-    command = command_format.format(name=project_name)
+    command = command_string.format(name=project_name)
     if dump_file is None:
         dump_file = open('db_%s.dump' % project_name, 'w')
     p = subprocess.Popen(command, shell=True, stdout=dump_file)
@@ -25,6 +36,12 @@ def dump(project_name, dump_file=None):
 
 
 def dump_all(dump_file=None):
+    """
+    Full dump
+    @type dump_file: file
+    @param dump_file: output file
+    """
+
     command = 'pg_dumpall'
     logger.info('Starting full dump')
 
@@ -38,8 +55,14 @@ def dump_all(dump_file=None):
 
 
 def generate_pgpass():
+    """
+    Generates a gpass file
+    @return: dictionary of database passwords
+    """
+
     logger.info('Generating pgpass file')
     config = get_config()
+    passwords = {}
     projects_folder = config.get('backuper', 'projects')
     user_root = os.path.expanduser("~")
     project_list = os.listdir(projects_folder)
@@ -50,22 +73,22 @@ def generate_pgpass():
         try:
             parser.read(os.path.join(projects_folder, project_name, 'conf', 'backuper.conf'))
             password = parser.get('backuper', 'password')
+            logger.info('Settings for project %s loaded' % project_name)
+            passwords.update({project_name: password})
         except (NoSectionError, IOError), e:
             logger.warning('Failed to process %s: %s' % (project_name, e))
             continue
-        pgpass_strings.append(password_format.format(key=password, name=project_name))
+        pgpass_strings.append(password_string.format(key=password, name=project_name))
 
     with open(os.path.join(user_root, '.pgpass'), 'w') as pgpass_file:
         pgpass_file.writelines(pgpass_strings)
 
     logger.info('Pgpass file generated. Processed projects: %s' % len(pgpass_strings))
-    return pgpass_strings
+    return passwords
 
 
 if __name__ == '__main__':
     settings = generate_pgpass()
-    if settings:
-        dump('machines')
-        dump('store')
-        #dump('discover')
+    for project in generate_pgpass():
+        dump(project)
     dump_all()
--- backuper/log.py
+++ backuper/log.py
@@ -3,7 +3,7 @@ import logging
 
 __ch = logging.StreamHandler()
 __ch.setLevel(logging.INFO)
-__formatter = logging.Formatter('%(asctime)s [%(levelname)s] <%(name)s> %(message)s', r'%d.%m.%y %H:%M:%S')
+__formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', r'%d.%m.%y %H:%M:%S')
 __ch.setFormatter(__formatter)
 
 
