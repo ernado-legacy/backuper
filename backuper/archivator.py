d3a12a88b73ea35295aefdbdb112632143fc7295
Ernado <ernado@cygame.ru>
2013-10-02 03:59:48 +0400

    added database compression

--- backuper/archivator.py
+++ backuper/archivator.py
@@ -7,6 +7,24 @@ from subprocess import call
 import log
 import errors
 
+def compress_file(input_file, output_file):
+    """
+    Compressing folder to file
+    @param input_folder: Folder to compress
+    @param output_file: output tar file
+    @param zip: compress flag
+    """
+    mode = 'w'
+    logger = log.get(__name__)
+    logger.info('Starting compression of the %s' % input_file)
+    if not os.path.isfile(input_file):
+        raise IOError('%s is not file or not found' % input_file)
+    mode = 'w:gz'
+    tar = tarfile.open(output_file, mode)
+    tar.add(input_file)
+    tar.close()
+    logger.info('File %s compressed to file %s' % (input_file, output_file))
+
 
 def compress(input_folder, output_file, zip=False):
     """
