87de57d21ae6ed370a219ae9b51a029153d50072
Ernado <ernado@cygame.ru>
2013-10-02 21:30:39 +0400

    added root settings to ftp

--- backuper/uploader.py
+++ backuper/uploader.py
@@ -15,7 +15,8 @@ def upload_files(file_list, cfg, logger):
     @param cfg: ParseConfig object
     """
     start_time = datetime.now()
-    uploads = dict(zip(file_list, map(lambda x: os.path.basename(x), file_list)))
+    root = cfg.get('ftp', 'root')
+    uploads = dict(zip(file_list, map(lambda x: os.path.join(root, os.path.basename(x)), file_list)))
     logger.info('Uploading files: %s' ' '.join(uploads.values()))
     total_size = 0
     # Checking files and counting
