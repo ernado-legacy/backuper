d275fc75a7ab0d126a4c1ccbf9df62e126d91d66
Ernado <ernado@cygame.ru>
2013-10-02 23:42:40 +0400

    incremental archive fix

--- backuper/archivator.py
+++ backuper/archivator.py
@@ -75,7 +75,7 @@ def incremental_compress(input_folder, output_file, incremental_list_file, log_f
     result = call(tar_call_arguments + ['--listed-incremental=%s' % incremental_list_file,
                                         '--file=%s' % output_file,
                                         input_folder],
-                  tdout=log_file,
+                  stdout=log_file,
                   stderr=log_file)
     if result == 0:
         logger.info('Archiving completed by %s seconds' % (datetime.now() - start_time).seconds)
